ae1da8bf910ee7d6e9d6e9e81aafe6bfe9857655
TTIT\bakp <pawel.bak@ttpsc.pl>
2020-12-08 14:41:26 +0100

    Added get request to the root server site. Will display welcome message

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome at site, that will list all available companies with name provided in body and will generate vcf file.');
+});
