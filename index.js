950d7467c90a55fbaf1380379ecc5908796b04a8
TTIT\bakp <pawel.bak@ttpsc.pl>
2020-12-08 14:46:29 +0100

    Prepared get endpoint that response with request body

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
   res.send('Welcome at site, that will list all available companies with name provided in body and will generate vcf file.');
 });
 
+app.get('/getCompanies', (req, res) => {
+  res.json({ result: req.body });
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
