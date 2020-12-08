b193a1ecb9e18d8ceb44e6e001246247d728255e
TTIT\bakp <pawel.bak@ttpsc.pl>
2020-12-08 14:41:55 +0100

    Added listener which will listen at localhost address at port 3000

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Welcome at site, that will list all available companies with name provided in body and will generate vcf file.');
 });
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
