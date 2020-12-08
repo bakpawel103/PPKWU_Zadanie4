88ee60fdceb3d06a2c2f023cad2f68db322110cc
TTIT\bakp <pawel.bak@ttpsc.pl>
2020-12-08 14:45:53 +0100

    Configured server to use cors

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Welcome at site, that will list all available companies with name provided in body and will generate vcf file.');
 });
